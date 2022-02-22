c0eb6462b75546ce0ccbd846a53937c064700cf3
Charan Skc <charanskc7@gmail.com>
2022-02-22 14:51:25 +0530

    final project and first commit

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+function Footer(){
+    return (
+        <div style={{backgroundColor:'black',textAlign:'center',color:'white',padding:"10px",borderRadius:'10px'}}>
+            <h3>A personal project by <a style={{color:'cyan',textDecoration:'none'}} href="mailto:charanskc7@gmai.com"> Sai Charan</a>.</h3>
+        </div>
+    );
+}
+
+export default Footer;
